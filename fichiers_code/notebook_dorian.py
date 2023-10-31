162ed9db2765fd2e813d4501e632cac5037b9fc7
Dorian Brousse <dorianbrousse@sfr.fr>
2023-10-31 17:13:15 +0100

    fin trav dorian

--- fichiers_code/notebook_dorian.py
+++ fichiers_code/notebook_dorian.py
@@ -25,13 +25,35 @@
 # sauvé en Python
 
 # %%
-def foo():
-    print("hello world")
-
-
-# %%
 import numpy as np
 import pandas as pd
+
+# %%
+REGIONS = {
+    'Auvergne-Rhône-Alpes': ['01', '03', '07', '15', '26', '38', '42', '43', '63', '69', '73', '74'],
+    'Bourgogne-Franche-Comté': ['21', '25', '39', '58', '70', '71', '89', '90'],
+    'Bretagne': ['35', '22', '56', '29'],
+    'Centre-Val de Loire': ['18', '28', '36', '37', '41', '45'],
+    'Corse': ['2A', '2B'],
+    'Grand Est': ['08', '10', '51', '52', '54', '55', '57', '67', '68', '88'],
+    'Guadeloupe': ['971'],
+    'Guyane': ['973'],
+    'Hauts-de-France': ['02', '59', '60', '62', '80'],
+    'Île-de-France': ['75', '77', '78', '91', '92', '93', '94', '95'],
+    'La Réunion': ['974'],
+    'Martinique': ['972'],
+    'Normandie': ['14', '27', '50', '61', '76'],
+    'Nouvelle-Aquitaine': ['16', '17', '19', '23', '24', '33', '40', '47', '64', '79', '86', '87'],
+    'Occitanie': ['09', '11', '12', '30', '31', '32', '34', '46', '48', '65', '66', '81', '82'],
+    'Pays de la Loire': ['44', '49', '53', '72', '85'],
+    'Provence-Alpes-Côte d\'Azur': ['04', '05', '06', '13', '83', '84'],
+}
+
+# %%
+dict_dep_reg = {}
+for (reg, ldep) in REGIONS.items():
+    for dep in ldep:
+        dict_dep_reg[dep]=reg
 
 # %%
 df = pd.read_excel('coiffeurs.xlsx')
@@ -40,14 +62,17 @@ df.drop(columns = ['type','type.1','markerinnerhtml','liinnerhtml'], inplace = T
 df
 
 # %%
-df['cpstr'] = df['codepostal'].str[:2]
+df['dep'] = df['codepostal'].str[:2]
+df
 
 # %%
+df['reg'] = df['dep'].map(dict_dep_reg)
+print(df)
+by_dep=df.groupby(['reg'])
 
 # %%
-df.info()
+df1 = by_dep.size()
+df1
 
 # %%
-# df.astype?
-
-# %%
+df1.plot.bar(label = 'nombre par région', xlabel='région', ylabel = 'nombre')
